2655fb795be2751b44a2fddcf8ccb7446fa1857f
Mario Carballo Zama <afk@ellugar.co>
2020-04-17 00:50:17 -0500

    Fix firebase credentials for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3rvCkI-8YymtHAy_k8XHOH8GvVyNqO7I',
+    authDomain: 'ddplayer-afk.firebaseapp.com',
+    databaseURL: 'https://ddplayer-afk.firebaseio.com',
+    projectId: 'ddplayer-afk',
+    storageBucket: 'ddplayer-afk.appspot.com',
+    messagingSenderId: '631215225802'
+  }
 };
